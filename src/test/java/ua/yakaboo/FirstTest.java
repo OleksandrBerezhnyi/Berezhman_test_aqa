9442855beeeb2ed896d563e066a25c8b7cb96078
Berezhman <oleksandr.berezhnyi@gravit.io>
2021-01-11 19:10:36 +0200

    Initial commit

---
+++ src/test/java/ua/yakaboo/FirstTest.java
@@ -0,0 +1,22 @@
+package ua.yakaboo;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class FirstTest {
+    @Test
+    public void firstTest() {
+        System.setProperty("webdriver.chrome.driver", "/Users/Berezhman/Downloads/chromedriver.exe");
+        ChromeDriver driver = new ChromeDriver();
+
+        driver.get("https://www.yakaboo.ua/");
+
+        String title = driver.getTitle();
+        System.out.println(driver.getTitle());
+        Assert.assertTrue(title.equals("Yakaboo — Інтернет-магазин книг, подарунків і дитячих товарів. Купити книги та подарунки по найкращим в Україні цінами."));
+
+        driver.quit();
+    }
+
+}
