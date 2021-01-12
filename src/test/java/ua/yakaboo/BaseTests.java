ca6b40d54fad20be355ca76e8f5403c4a0cd99ab
Berezhman <oleksandr.berezhnyi@gravit.io>
2021-01-12 11:57:00 +0200

    Updated

--- src/test/java/ua/yakaboo/BaseTests.java
+++ src/test/java/ua/yakaboo/BaseTests.java
@@ -1,28 +1,27 @@
 package ua.yakaboo;
 
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import org.openqa.selenium.By;
 
 public class BaseTests extends WebDriverSettings {
 
     @Test
     public void homePageIsDisplayed() {
-        driver.get("https://www.yakaboo.ua/");
-
         String title = driver.getTitle();
         Assert.assertTrue(title.equals("Yakaboo — Інтернет-магазин книг, подарунків і дитячих товарів. Купити книги та подарунки по найкращим в Україні цінами."));
     }
 
     @Test
-    public void SignIn() {
-        driver.get("https://www.yakaboo.ua/");
-
+    public void signInWithValidUser() throws InterruptedException {
         driver.findElement(By.id("customer_menu")).click();
-        driver.findElement(By.id("modal-login")).isDisplayed();
+        driver.findElement(By.id("modal-login"));
+        Thread.sleep(3000);
+        Assert.assertTrue(driver.findElement(By.id("modal-login")).isDisplayed());
+
         driver.findElement(By.id("email")).sendKeys("oleksandr.berezhnyi@gravit.io");
         driver.findElement(By.id("pass")).sendKeys("123456");
         driver.findElement(By.id("button-login-modal")).click();
     }
 }
+
--- src/test/java/ua/yakaboo/WebDriverSettings.java
+++ src/test/java/ua/yakaboo/WebDriverSettings.java
@@ -1,20 +1,20 @@
 package ua.yakaboo;
 
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.*;
 
 public class WebDriverSettings {
 
     public ChromeDriver driver;
 
-    @BeforeTest
+    @BeforeMethod
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver87");
         driver = new ChromeDriver();
+        driver.get("https://www.yakaboo.ua/");
     }
 
-    @AfterTest
+    @AfterMethod
     public void close() {
         driver.quit();
     }
